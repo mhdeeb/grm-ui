7a0da1c67da6513f085eaa7e4963ac9a22ca2b39
Mohamed Hussien El-Deeb <s-mohamed.eldeeb@zewailcity.edu.eg>
2024-06-10 21:42:14 +0300

    test

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -2,7 +2,7 @@ export default function MainContent() {
     return (
         <main>
             <h1>Welcome to the Home Page</h1>
-            <p>This is the home page of the website.</p>
+            <p>This is the home page of the website. test</p>
         </main>
     );
 }
