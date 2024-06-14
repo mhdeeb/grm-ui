11e18c7b722681b35f504da4b75b4a2e552396a9
Mohamed Hussien El-Deeb <s-mohamed.eldeeb@zewailcity.edu.eg>
2024-06-14 16:45:48 +0300

    WIP: Navigation menu

---
+++ src/components/ListIcon.tsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom"
+
+const style = {
+    display: 'flex',
+    alignItems: 'center',
+}
+
+const imgStyle = {
+    width: '1.5rem',
+    height: '1.5rem',
+    marginRight: '10px',
+}
+
+export default function ListIcon(props: any) {
+    return (
+        <li className={props.className}>        
+            <NavLink style={style} to={props.to}><img style={imgStyle} src={props.src} alt={props.alt} />{props.text}</NavLink>
+        </li>
+    )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,22 @@
-import { NavLink } from 'react-router-dom';
-import TitleImage from '../asset/favicon.ico';
-import DataBaseImage from '../asset/database.svg';
-import DataPreprocessing from '../asset/processing.svg';
-import KoverLearnImage from '../asset/kover.svg';
-import AnalysisImage from '../asset/analysis.svg';
+import ListIcon from '../components/ListIcon';
+import TitleIcon from '../asset/favicon.ico';
+import DataBaseIcon from '../asset/database.svg';
+import DataPreprocessingIcon from '../asset/processing.svg';
+import KoverLearnIcon from '../asset/kover.svg';
+import AnalysisIcon from '../asset/analysis.svg';
+import SettingsIcon from '../asset/settings.svg';
 import "../style/index.css";
 
 export default function Navbar() {
     return (
         <nav className="nav-bar">
-            <h1 className='title'><img src={TitleImage} alt="" />GRM</h1>
+            <h1 className='title'><img src={TitleIcon} alt="" />GRM</h1>
             <ul>
-                <li>
-                    
-                    <NavLink to="/collection"><img src={DataBaseImage} alt="" />Data collection</NavLink>
-                </li>
-                <li>
-                
-                <NavLink to="/preprocessing"><img src={DataPreprocessing} alt="" />Data preprocessing</NavLink></li>
-                <li>
-                
-                <NavLink to="/kover"><img src={KoverLearnImage} alt="" />Kover learn</NavLink></li>
-                <li>
-                
-                <NavLink to="/analysis"><img src={AnalysisImage} alt="" />Analysis</NavLink></li>
-                <li className='bottom'><NavLink to="/settings">Settings</NavLink></li>
+                <ListIcon to="/collection" src={DataBaseIcon} alt="Data Collection" text="Data Collection"/>
+                <ListIcon to="/preprocessing" src={DataPreprocessingIcon} alt="Data preprocessing" text="Data preprocessing"/>
+                <ListIcon to="/kover" src={KoverLearnIcon} alt="Kover learn" text="Kover learn"/>
+                <ListIcon to="/analysis" src={AnalysisIcon} alt="Analysis" text="Analysis"/>
+                <ListIcon className="bottom" to="/settings" src={SettingsIcon} alt="Settings" text="Settings"/>
             </ul>
         </nav>
     );
