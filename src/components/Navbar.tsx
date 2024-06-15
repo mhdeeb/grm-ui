7eea19ed7e3f7b71949444e016fd34b0939c565a
Mohamed Hussien El-Deeb <s-mohamed.eldeeb@zewailcity.edu.eg>
2024-06-15 15:57:09 +0300

    WIP: Navigation menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,10 +7,25 @@ import AnalysisIcon from '../asset/analysis.svg';
 import SettingsIcon from '../asset/settings.svg';
 import "../style/index.css";
 
+const titleStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    margin: '12.5% 0 6.25% 25%',
+    "user-select": 'none',
+    "font-size": '1.5rem',
+    "font-weight": 'bold',
+}
+
+const titleImgStyle = {
+    width: '2rem',
+    height: '2rem',
+    marginRight: '10px',
+}
+
 export default function Navbar() {
     return (
         <nav className="nav-bar">
-            <h1 className='title'><img src={TitleIcon} alt="" />GRM</h1>
+            <h1 style={titleStyle}><img style={titleImgStyle} src={TitleIcon} alt="GRM" />GRM</h1>
             <ul>
                 <ListIcon to="/collection" src={DataBaseIcon} alt="Data Collection" text="Data Collection"/>
                 <ListIcon to="/preprocessing" src={DataPreprocessingIcon} alt="Data preprocessing" text="Data preprocessing"/>
