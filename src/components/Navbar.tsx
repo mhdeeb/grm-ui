d497ce65d79e075fd524d53b21e020df96720506
Mohamed Hussien El-Deeb <s-mohamed.eldeeb@zewailcity.edu.eg>
2024-06-19 19:06:11 +0300

    Finish: Navigation menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,31 +7,32 @@ import AnalysisIcon from '../asset/analysis.svg';
 import SettingsIcon from '../asset/settings.svg';
 import "../style/index.css";
 
-const titleStyle = {
+const titleStyle: any = {
     display: 'flex',
     alignItems: 'center',
-    margin: '12.5% 0 6.25% 25%',
-    "user-select": 'none',
-    "font-size": '1.5rem',
-    "font-weight": 'bold',
+    margin: '12.5% 0 12.5% 25%',
+    userSelect: 'none'
 }
 
-const titleImgStyle = {
+const titleImgStyle: any = {
     width: '2rem',
     height: '2rem',
-    marginRight: '10px',
+    marginRight: '15px',
 }
+
+const FlexSpacer = () => <div style={{flex: '1'}}></div>;
 
 export default function Navbar() {
     return (
         <nav className="nav-bar">
-            <h1 style={titleStyle}><img style={titleImgStyle} src={TitleIcon} alt="GRM" />GRM</h1>
-            <ul>
+            <h1 style={titleStyle}><img draggable="false" style={titleImgStyle} src={TitleIcon} alt="GRM" />GRM</h1>
+            <ul className='nav-content'>
                 <ListIcon to="/collection" src={DataBaseIcon} alt="Data Collection" text="Data Collection"/>
                 <ListIcon to="/preprocessing" src={DataPreprocessingIcon} alt="Data preprocessing" text="Data preprocessing"/>
                 <ListIcon to="/kover" src={KoverLearnIcon} alt="Kover learn" text="Kover learn"/>
                 <ListIcon to="/analysis" src={AnalysisIcon} alt="Analysis" text="Analysis"/>
-                <ListIcon className="bottom" to="/settings" src={SettingsIcon} alt="Settings" text="Settings"/>
+                <FlexSpacer />
+                <ListIcon to="/settings" src={SettingsIcon} alt="Settings" text="Settings"/>
             </ul>
         </nav>
     );
